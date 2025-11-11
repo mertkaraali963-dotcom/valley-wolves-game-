66965d457faf7e28f163b3bc7735c575740ba97e
mertkaraali963-dotcom <mertkaraali963@gmail.com>
2025-11-11 14:43:18 +0200

    Create main.jsx Add main React file

---
+++ public/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MobileGuessGame from "./components/MobileGuessGame";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MobileGuessGame />
+  </React.StrictMode>
+);
